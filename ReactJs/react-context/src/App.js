bc8d0bde41c287cff8393b11082b42d1a2a07fbf
vinaybudideti <vinaykumarreddy.budideti@gmail.com>
2025-04-02 21:32:13 -0500

    Update App.js

--- ReactJs/react-context/src/App.js
+++ ReactJs/react-context/src/App.js
@@ -1,27 +1,113 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
-import Header from './components/Header'
-import LandingSection from './components/LandingSection'
-import FeaturesSection from './components/FeaturesSection'
-import LanguageContext from './context/LanguageContext'
+import ContactItem from './components/ContactItem'
+
+import './App.css'
+
+const initialContactsList = [
+  {
+    id: uuidv4(),
+    name: 'Ram',
+    mobileNo: 9999988888,
+    isFavorite: false,
+  },
+  {
+    id: uuidv4(),
+    name: 'Pavan',
+    mobileNo: 8888866666,
+    isFavorite: true,
+  },
+  {
+    id: uuidv4(),
+    name: 'Nikhil',
+    mobileNo: 9999955555,
+    isFavorite: false,
+  },
+]
 
 class App extends Component {
-  state = {activeLanguage: 'EN'}
+  state = {
+    contactsList: initialContactsList,
+    name: '',
+    mobileNo: '',
+  }
 
-  changeLanguage = activeLanguage => {
-    this.setState({activeLanguage})
+  toggleIsFavorite = id => {
+    this.setState(prevState => ({
+      contactsList: prevState.contactsList.map(eachContact => {
+        if (id === eachContact.id) {
+          return {...eachContact, isFavorite: !eachContact.isFavorite}
+        }
+        return eachContact
+      }),
+    }))
+  }
+
+  onAddContact = event => {
+    event.preventDefault()
+    const {name, mobileNo} = this.state
+    const newContact = {
+      id: uuidv4(),
+      name,
+      mobileNo,
+      isFavorite: false,
+    }
+
+    this.setState(prevState => ({
+      contactsList: [...prevState.contactsList, newContact],
+      name: '',
+      mobileNo: '',
+    }))
+  }
+
+  onChangeMobileNo = event => {
+    this.setState({mobileNo: event.target.value})
+  }
+
+  onChangeName = event => {
+    this.setState({name: event.target.value})
   }
 
   render() {
-    const {activeLanguage} = this.state
+    const {name, mobileNo, contactsList} = this.state
     return (
-      <LanguageContext.Provider
-        value={{activeLanguage, changeLanguage: this.changeLanguage}}
-      >
-        <Header />
-        <LandingSection />
-        <FeaturesSection />
-      </LanguageContext.Provider>
+      <div className="app-container">
+        <div className="responsive-container">
+          <h1 className="heading">Contacts</h1>
+          <form className="contact-form-container" onSubmit={this.onAddContact}>
+            <input
+              value={name}
+              onChange={this.onChangeName}
+              className="input"
+              placeholder="Name"
+            />
+            <input
+              className="input"
+              value={mobileNo}
+              onChange={this.onChangeMobileNo}
+              placeholder="Mobile Number"
+            />
+            <button type="submit" className="button">
+              Add Contact
+            </button>
+          </form>
+          <ul className="contacts-table">
+            <li className="table-header">
+              <p className="table-header-cell name-column">Name</p>
+              <hr className="separator" />
+              <p className="table-header-cell">Mobile Number</p>
+            </li>
+            {contactsList.map(eachContact => (
+              <ContactItem
+                key={eachContact.id}
+                contactDetails={eachContact}
+                toggleIsFavorite={this.toggleIsFavorite}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
     )
   }
 }
