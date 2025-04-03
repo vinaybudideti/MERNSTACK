95869b595c829eeab69ff06bd14719d64d0c647c
vinaybudideti <vinaykumarreddy.budideti@gmail.com>
2025-04-02 21:34:14 -0500

    Update index.js

--- ReactJs/contacts-app/src/components/ContactItem/index.js
+++ ReactJs/contacts-app/src/components/ContactItem/index.js
@@ -1,12 +1,16 @@
 import './index.css'
 
 const ContactItem = props => {
-  const {contactDetails} = props
-  const {name, mobileNo, isFavorite} = contactDetails
+  const {contactDetails, toggleIsFavorite} = props
+  const {name, mobileNo, isFavorite, id} = contactDetails
 
   const starImgUrl = isFavorite
     ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  const onClickFavoriteIcon = () => {
+    toggleIsFavorite(id)
+  }
 
   return (
     <li className="table-row">
@@ -16,7 +20,11 @@ const ContactItem = props => {
       <hr className="separator" />
       <div className="table-cell mobile-no-column">
         <p className="mobile-no-value">{mobileNo}</p>
-        <button type="button" className="favorite-icon-container">
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={onClickFavoriteIcon}
+        >
           <img src={starImgUrl} className="favorite-icon" alt="star" />
         </button>
       </div>
